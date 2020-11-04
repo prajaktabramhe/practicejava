d973280eca471d1b3b09cf3b68275d8f72f74898
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-04 15:53:13 +0530

    add number

--- src/practice/helloworld.java
+++ src/practice/helloworld.java
@@ -2,6 +2,13 @@ package practice;
 
 public class helloworld {
 	public static void main(String args[]){  
+		int first = 10;
+        int second = 20;
+
 	     System.out.println("Hello Java");  
+	     System.out.println("Enter two numbers: " + first + " " + second);
+	        int sum = first + second;
+
+	        System.out.println("The sum is: " + sum);
 	    }  
 }
