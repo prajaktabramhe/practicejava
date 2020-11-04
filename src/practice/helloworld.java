8598b9a852919b4aaf98ee0821ebfb2388993068
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-04 16:07:10 +0530

    multiplication

--- src/practice/helloworld.java
+++ src/practice/helloworld.java
@@ -1,14 +1,19 @@
 package practice;
 
 public class helloworld {
-	public static void main(String args[]){  
+	public static void main(String args[]){ 
+		
+
 		int first = 10;
         int second = 20;
 
 	     System.out.println("Hello Java");  
 	     System.out.println("Enter two numbers: " + first + " " + second);
 	        int sum = first + second;
-
+	        int mul = first * second;
 	        System.out.println("The sum is: " + sum);
+	        System.out.println("The mul is: " + mul);
+	        
+	        
 	    }  
 }
